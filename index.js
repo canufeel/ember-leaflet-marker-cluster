4d39347bdf2754213ad066159ad6a0a6eba2e6b4
Dariusz Niemczyk <palid@o2.pl>
2016-01-06 16:35:25 +0100

    Fix paths for bower. Fixes #2

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 module.exports = {
   name: 'ember-leaflet-marker-cluster',
   included: function(app){
-  	app.import(app.bowerDirectory + '/Leaflet.markercluster/dist/leaflet.markercluster.js');
-  	app.import(app.bowerDirectory + '/Leaflet.markercluster/dist/MarkerCluster.css');
+  	app.import(app.bowerDirectory + '/leaflet.markercluster/dist/leaflet.markercluster.js');
+  	app.import(app.bowerDirectory + '/leaflet.markercluster/dist/MarkerCluster.css');
   	app.import(app.bowerDirectory + '/leaflet.markercluster/dist/MarkerCluster.Default.css');
   }
 };
