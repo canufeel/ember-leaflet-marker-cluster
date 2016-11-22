f223e5c32c456c08f441b36bd8f13758fab1d356
willviles <will@willviles.com>
2016-11-22 15:05:10 +0000

    Adds FastBoot compatibility

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ module.exports = {
   name: 'ember-leaflet-marker-cluster',
 
   included: function(app){
-  	app.import(app.bowerDirectory + '/leaflet.markercluster/dist/leaflet.markercluster.js');
+
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+  	  app.import(app.bowerDirectory + '/leaflet.markercluster/dist/leaflet.markercluster.js');
+    }
+
   	app.import(app.bowerDirectory + '/leaflet.markercluster/dist/MarkerCluster.css');
   	app.import(app.bowerDirectory + '/leaflet.markercluster/dist/MarkerCluster.Default.css');
+    
   }
 };
