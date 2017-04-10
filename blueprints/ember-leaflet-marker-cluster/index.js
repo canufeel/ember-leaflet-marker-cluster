ce0148a4a58c85bcc0ace87a8f5b0d52cc77c953
Will Viles <will@willviles.com>
2017-04-10 16:56:24 +0100

    Moves from bower to npm for sourcing leaflet.markercluster

--- blueprints/ember-leaflet-marker-cluster/index.js
+++ blueprints/ember-leaflet-marker-cluster/index.js
@@ -1,21 +1,9 @@
 /*jshint node:true*/
 module.exports = {
-  description: 'pull leaflet.markercluster assets using bower',
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
   normalizeEntityName: function() {
    // this prevents an error when the entityName is
    // not specified (since that doesn't actually matter
    // to us
-  },
-
-  afterInstall: function(options) {
-   return this.addBowerPackageToProject('leaflet.markercluster', '~1.0.0');
   }
+
 };
--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var path = require('path');
+
 module.exports = {
   name: 'ember-leaflet-marker-cluster',
 
-  included: function(app){
+  included(app) {
 
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-  	  app.import(app.bowerDirectory + '/leaflet.markercluster/dist/leaflet.markercluster.js');
+    if (!isFastBoot()) {
+
+      const vendor = this.treePaths.vendor;
+
+      // Main modules
+      app.import(vendor + '/leaflet.markercluster/leaflet.markercluster.js');
+      app.import(vendor + '/leaflet.markercluster/MarkerCluster.css');
+      app.import(vendor + '/leaflet.markercluster/MarkerCluster.Default.css');
+
     }
 
-  	app.import(app.bowerDirectory + '/leaflet.markercluster/dist/MarkerCluster.css');
-  	app.import(app.bowerDirectory + '/leaflet.markercluster/dist/MarkerCluster.Default.css');
-    
-  }
+    return this._super.included.apply(this, arguments);
+  },
+
+  treeForVendor(vendorTree) {
+    var trees = [];
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(moduleToFunnel('leaflet.markercluster', {
+      include: ['*.js', '*.css'],
+      destDir: 'leaflet.markercluster'
+    }));
+
+    return mergeTrees(trees);
+  },
+
 };
+
+function moduleToFunnel(moduleName, opts) {
+  opts = opts || { destDir: moduleName };
+  return new Funnel(resolveModulePath(moduleName), opts);
+}
+
+function resolveModulePath(moduleName) {
+  return path.dirname(require.resolve(moduleName));
+}
+
+// Checks to see whether this build is targeting FastBoot. Note that we cannot
+// check this at boot time--the environment variable is only set once the build
+// has started, which happens after this file is evaluated.
+function isFastBoot() {
+  return process.env.EMBER_CLI_FASTBOOT === 'true';
+}
