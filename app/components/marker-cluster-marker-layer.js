383cf5a064c39078b57184156c0b7fa1b04ee4e8
Matt Wisner <matt@detroitweb.company>
2018-04-23 16:24:55 -0400

    Adding files

---
+++ app/components/marker-cluster-marker-layer.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-leaflet-marker-cluster/components/marker-cluster-marker-layer';
---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,90 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  lat: 42.963360,
+  lng: -85.668086,
+  zoom: 10,
+  maxZoom: 15,
+  markers: Object.freeze([
+    {
+      title: "Test A",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test B",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test C",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+    {
+      title: "Test",
+      location: [42.963360,-85.668086],
+    },
+  ]),
+});
