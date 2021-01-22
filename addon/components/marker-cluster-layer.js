1ac1d864e4322e770c70b86abd06555f96ee80d1
Miguel Andrade <jmandrade0@gmail.com>
2021-01-22 02:00:17 +0000

    update to glimmer components

--- addon/components/marker-cluster-layer.js
+++ addon/components/marker-cluster-layer.js
@@ -1,27 +1,24 @@
 import BaseLayer from 'ember-leaflet/components/base-layer';
-import { ParentMixin } from 'ember-composability-tools';
-import layout from '../templates/marker-cluster-layer';
 
-export default BaseLayer.extend(ParentMixin, {
-	layout,
+export default class MarkerClusterLayer extends BaseLayer {
+    leafletOptions = [
+        ...this.leafletOptions, 'showCoverageOnHover', 'zoomToBoundsOnClick', 'spiderfyOnMaxZoom', 'removeOutsideVisibleBounds',
+        'animate', 'animateAddingMarkers', 'disableClusteringAtZoom', 'maxClusterRadius', 'polygonOptions',
+        'singleMarkerMode', 'spiderLegPolylineOptions', 'spiderfyDistanceMultiplier', 'iconCreateFunction'
+    ];
 
-	leafletOptions: Object.freeze([
-    'showCoverageOnHover', 'zoomToBoundsOnClick', 'spiderfyOnMaxZoom', 'removeOutsideVisibleBounds',
-    'animate', 'animateAddingMarkers', 'disableClusteringAtZoom', 'maxClusterRadius', 'polygonOptions',
-    'singleMarkerMode', 'spiderLegPolylineOptions', 'spiderfyDistanceMultiplier', 'iconCreateFunction'
-	]),
-
-	leafletEvents: Object.freeze([
-    // Marker clutster events
-    'clusterclick', 'clusterdblclick', 'clustermousedown', 'clustermouseover', 'clustermouseout',
-    'clustercontextmenu', 'clusteradd', 'clusterremove', 'animationend', 'spiderfied', 'unspiderfied',
-    // Marker events
-    'click', 'dblclick', 'mousedown', 'mouseover', 'mouseout',
-    'contextmenu', 'dragstart', 'drag', 'dragend', 'move', 'remove', 'add',
-    'popupopen', 'popupclose'
-	]),
+	leafletEvents = [
+        ...this.leafletEvents,
+        // Marker clutster events
+        'clusterclick', 'clusterdblclick', 'clustermousedown', 'clustermouseover', 'clustermouseout',
+        'clustercontextmenu', 'clusteradd', 'clusterremove', 'animationend', 'spiderfied', 'unspiderfied',
+        // Marker events
+        'click', 'dblclick', 'mousedown', 'mouseover', 'mouseout',
+        'contextmenu', 'dragstart', 'drag', 'dragend', 'move', 'remove', 'add',
+        'popupopen', 'popupclose'
+	];
 
 	createLayer() {
-		return this.L.markerClusterGroup(...this.get('requiredOptions'), this.get('options'));
+		return this.L.markerClusterGroup(...this.requiredOptions, this.options);
 	}
-});
+}
